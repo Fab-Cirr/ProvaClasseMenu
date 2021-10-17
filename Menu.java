0ea84a0a5d092db399ade40b23f1f7d8ecfaabe0
Orazio Fabio Cirrottola <78730939+Fab-Cirr@users.noreply.github.com>
2021-10-17 20:16:00 +0000

    iola

--- Menu.java
+++ Menu.java
@@ -21,7 +21,7 @@ while(!flagMenu){
 
   if(scelta == 1){
     System.out.println("La prima scelta");
-    System.out..println("questa è la stampa sul main branch! ");
+    System.out.println("questa è la stampa sul main branch! ");
     
 
   }else if(scelta == 2){
